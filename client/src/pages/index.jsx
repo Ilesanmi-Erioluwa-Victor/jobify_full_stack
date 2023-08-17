c00a1cc26f81cfdce8b0b71150d378a4e4b5c109
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 09:00:08 +0100

    Working on router

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,2 +1,4 @@
 export { default as DashboardLayout } from './DashboardLayout';
 export { default as Admin } from './Admin';
+export { default as AllJobs } from './AllJobs';
+export { default as DeleteJob } from './DeleteJob';
