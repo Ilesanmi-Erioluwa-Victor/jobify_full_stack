0c027a5abbd219154ee0a537c654609e5591a3bd
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 09:02:58 +0100

    Working on router

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -8,3 +8,5 @@ export { default as Error } from './Error';
 export { default as HomeLayout } from './HomeLayout';
 export { default as Landing } from './Landing';
 export { default as Login } from './Login';
+export { default as Profile } from './Profile';
+export { default as Register } from './Register';
