c478256dd1ec69712d33f2101236d38e1dedf834
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 09:11:26 +0100

    Working on router

---
+++ client/src/pages/pageMap.jsx
@@ -0,0 +1,11 @@
+import {
+    Admin, DashboardLayout, DeleteJob,
+    EditJob, AddJob, AllJobs, Error,
+    HomeLayout, Landing, Login,
+    Register, Profile, Stats
+} from "./index"
+
+export const pageMap = [{
+    path: "/",
+    render : 
+}]
