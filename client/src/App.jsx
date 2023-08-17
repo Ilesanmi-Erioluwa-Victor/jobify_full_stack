520fc710b143252f11f1cd5e96b1092d3b73d8c2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 06:44:09 +0100

    Refactoring codes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
+import { createBrowser } from ' react-router-dom';
 
 const App = () => {
-  return (
-    <h1>
-      Jobify App
-    </h1>
-  )
-}
+  return <h1>Jobify App</h1>;
+};
 
-export default App
-
+export default App;
