acb7ed8af4cf8f893e273ec488d3d2d88af1398a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 08:31:36 +0100

    Working on router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom"
 
 const router = createBrowserRouter([{
-  path: "/"
+  path: "/",
+  element: <h1>Home Page</h1>
 }]);
 
 const App = () => {
