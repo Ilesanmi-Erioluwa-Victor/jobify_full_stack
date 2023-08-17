ef9e834b2dd3a997b60085fb6aa032dc0f5ec998
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 08:32:43 +0100

    Working on router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router-dom"
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <h1>Home Page</h1>
-}]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home Page</h1>,
+  },
+
+  {
+    path: '/',
+    element: <h1>Home Page</h1>,
+  },
+
+  {
+    path: '/about',
+    element: <h1>About Us Page</h1>,
+  },
+
+  {
+    path: '/contact',
+    element: <h1>Contact us Page</h1>,
+  },
+]);
 
 const App = () => {
   return <h1>Jobify App</h1>;
