a14c70d97cf2c103cb36bd009a8cb690602876ee
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 10:29:16 +0100

    working on Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,12 @@
+import Wrapper from 'assets/wrappers/LandingPage';
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>Nav Bar</nav>
+      <h1>Landing Page </h1>
+    </Wrapper>
+  );
 };
 
 export default Landing;
