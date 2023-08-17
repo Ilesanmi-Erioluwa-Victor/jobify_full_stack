44c6fea55f602f7d7885ba807dc57207beec6fbf
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-17 10:46:22 +0100

    working on Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -5,6 +5,9 @@ import logo from 'assets/images/logo.svg';
 const Landing = () => {
   return (
     <Wrapper>
+      <nav>
+        <img src={logo} alt="Logo" />
+      </nav>
       <h1>Landing Page </h1>
     </Wrapper>
   );
