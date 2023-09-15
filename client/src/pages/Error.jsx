8cea8b4f3f5d3346c8682b448826532b73a39675
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-15 23:45:10 +0100

    working on error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link , useRouteError} from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage"
+import img from "../assets/images/not-found.svg"
 
 const Error = () => {
+  console.log(error)
   return <div>
     <h1>Error</h1>
     <Link to={"/"}>Back to Home</Link>
