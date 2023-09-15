a1b1b68217485bb839346df4e40dde90dfc276a0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 00:02:04 +0100

    working on error

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -4,13 +4,26 @@ import img from '../assets/images/not-found.svg';
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img
+            src={img}
+            alt='not found'
+          />
+          <h3>Ohh page not found</h3>
+          <p>We cant seem to find the page you are looking for</p>
+          <Link to={'/dashboard'}>Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
     <Wrapper>
       <div>
-        <img src={img} alt="not found" />
         <h3>Ohh page not fo</h3>
-        <Link to={'/'}>Back to Home</Link>
       </div>
     </Wrapper>
   );
