fb61796f74ee9d20727e9479398ff5e9e488a457
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 00:11:41 +0100

    working on Register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { Logo } from "../components"
 
 const Register = () => {
   return (
-    <div>
+    <Wrapper>
+      <form className="form"></form>
       <h1>Register</h1>
-      <Link to={'/login'}>Login to your Account</Link>
-    </div>
+    </Wrapper>
   );
 };
 
