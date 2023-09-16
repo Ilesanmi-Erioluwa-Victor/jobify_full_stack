39ea234afac90cf7868b153f72ba9081533a05c8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 15:01:07 +0100

    working on Navlinks

--- client/src/components/BigSideBar.jsx
+++ client/src/components/BigSideBar.jsx
@@ -1,4 +1,6 @@
 import Wrapper from 'assets/wrappers/BigSidebar';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 
 const BigSidebar = () => {
   return <Wrapper><h3>BigSidebar</h3></Wrapper>;
