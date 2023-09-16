4090b1a42e7eebde8b6e1ef7cfabe9cfdc59838e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 15:02:09 +0100

    working on BigSidebar

--- client/src/components/BigSideBar.jsx
+++ client/src/components/BigSideBar.jsx
@@ -1,8 +1,11 @@
 import Wrapper from 'assets/wrappers/BigSidebar';
 import NavLinks from './NavLinks';
 import Logo from './Logo';
+import { useDashboardContext } from 'pages/DashboardLayout';
+
 
 const BigSidebar = () => {
+  const { showSidebar } = useDashboardContext();
   return <Wrapper><h3>BigSidebar</h3></Wrapper>;
 };
 export default BigSidebar;
