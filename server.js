784acf84060a529dd4dec6b693c50226b0ded155
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 22:40:17 +0100

    working on app

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import morgan from "morgan"
 
 const app = express();
 
 app.use(express.json());
-
-
-
+app.use(morgan("dev"))
 app.get('/', (req, res, next) => {
   res.send('Hello world');
+});
+
+app.post('/', (req, res, next) => {
+  console.log(req);
 });
 
 const PORT = 8080;
