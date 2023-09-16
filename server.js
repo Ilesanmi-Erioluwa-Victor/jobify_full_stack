2bb3e0aaebd8879cfc1484899e60f9ad87264f7d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 22:47:15 +0100

    working on app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import morgan from "morgan"
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"))
-
+app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
   res.send('Hello world');
