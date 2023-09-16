73128e3f40fb65d97d1f9cdc4ba3229a4ca4d2a6
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 02:28:52 +0100

    working on DashboardLayout

--- client/src/components/SmallSideBar.jsx
+++ client/src/components/SmallSideBar.jsx
@@ -1,20 +1,28 @@
 import Wrapper from 'assets/wrappers/SmallSidebar';
 import { useDashboardContext } from 'pages/DashboardLayout';
-import { FaTimes } from "react-icons/fa"
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+
 const SmallSidebar = () => {
   const data = useDashboardContext();
 
-  return <Wrapper>
-    <div className="sidebar-container show-sidebar">
-      <div className="content">
-        <button type="button" className="close-btn">
-        <FaTimes  />
-        </button>
-
-        
-   </div>
-    </div>
-  </Wrapper>;
+  return (
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button
+            type='button'
+            className='close-btn'
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
