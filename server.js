d753f0959a5b9dffdfe27cfba597c51ae0800d81
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 22:23:27 +0100

    working on app

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res, next) =>{
+    res.send("Hello world")
+})
+
+const PORT = 8080
+
+app.listen(PORT || 8080, () =>{
+    console.log("server running on +", PORT)
+})
