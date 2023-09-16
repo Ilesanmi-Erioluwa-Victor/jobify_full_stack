1f0e1f51e7884e70433557fa4608e22eaf4435b5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 15:00:09 +0100

    working on Navlinks

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -19,6 +19,7 @@ const NavLinks = () => {
             onClick={toggleSidebar}
           >
             <span className='icon'>{icon}</span>
+            {text}
           </NavLink>
         );
       })}
