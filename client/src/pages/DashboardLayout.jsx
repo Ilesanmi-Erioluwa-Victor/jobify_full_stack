cd6705920673121aa4516f1bc00a74813855c044
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-16 01:19:10 +0100

    working on DashboardLayout

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,10 +1,14 @@
+import { useState} from "react"
 import { Outlet } from 'react-router';
 import Wrapper from 'assets/wrappers/Dashboard';
 import { SmallSidebar, BigSidebar, Navbar } from 'components';
 
 const DashboardLayout = () => {
+  const user = { name: "John" };
 
-  
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
     <Wrapper>
       <main className='dashboard'>
