a8d8c58051f46b95c67aec30e43684a1ea9e9f3a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 22:23:16 +0100

    working on req.body validation

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -6,11 +6,18 @@ import {
   getAllJobs,
   getJob,
 } from '../controller/jobController.js';
-import { validateJobInput } from '../middleware/validationMiddleware.js';
+import {
+  validateJobInput,
+  validateParam,
+} from '../middleware/validationMiddleware.js';
 
 const route = Router();
 
 route.route('/').get(getAllJobs).post(validateJobInput, createJob);
-route.route('/:id').get(getJob).patch(validateJobInput,editJob).delete(deleteJob);
+route
+  .route('/:id')
+  .get(validateParam, getJob)
+  .patch(validateJobInput, editJob)
+  .delete(validateParam, deleteJob);
 
 export default route;
