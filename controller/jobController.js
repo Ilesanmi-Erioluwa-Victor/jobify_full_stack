9155afd19724e7cef3df6ad23dac672eee7d179a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 16:36:40 +0100

    working on jobContoller

--- controller/jobController.js
+++ controller/jobController.js
@@ -21,20 +21,6 @@ export const getAllJobs = async (req, res, next) => {
 export const createJob = async (req, res, next) => {
   const { company, position } = req.body;
 
-  if (!company || !position) {
-    return res.status(400).json({
-      status: 'failed',
-      msg: 'Please, provide all the required fields',
-    });
-  }
-
-  const id = nanoid(10);
-  const job = {
-    id,
-    company,
-    position,
-  };
-  jobs.push(job);
   res.json({ status: 'success', message: 'ok', data: job });
 };
 
