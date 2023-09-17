64f53ea477fcd8ba0ecda257b9bc653873f534e1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 21:12:41 +0100

    working on req.body validation

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ app.post(
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const errorMessages = errors.array() 
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({
+        errors : errorMessages
+      })
     }
     console.log(errors.isEmpty());
 
