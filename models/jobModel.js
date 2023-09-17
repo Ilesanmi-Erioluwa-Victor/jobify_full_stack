0f68dd65b2f4aae6b5470e533f331b5f38cc8a69
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 16:28:10 +0100

    working on jobSchema

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,17 +1,23 @@
 import mongoose from 'mongoose';
 
-const JobSchema = new mongoose.Schema({
-  company: String,
-  location: String,
-  jobStatus: {
-    type: String,
-    enum: ['interview', 'declined', 'pending'],
-    default: 'pending',
-  },
-  jobType: {
-    type: String,
-    enum: ['full-time', 'part-time', 'internship'],
-    default: 'full-time',
+const JobSchema = new mongoose.Schema(
+  {
+    company: String,
+    location: String,
+    jobStatus: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
     },
-  
-});
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      default: 'My city',
+    },
+  },
+  { timestamps: true }
+);
