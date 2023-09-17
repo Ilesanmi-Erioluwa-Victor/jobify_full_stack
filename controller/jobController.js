31add65e81a3dab7a7c6babbad393e2c71784483
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 22:41:19 +0100

    working on req.body validation

--- controller/jobController.js
+++ controller/jobController.js
@@ -1,8 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/jobModel.js';
 
-import { NotFoundError } from '../errors/customError.js';
-
 export const getAllJobs = async (req, res, next) => {
   const jobs = await Job.find();
   res.status(StatusCodes.OK).json({
@@ -14,7 +12,6 @@ export const getAllJobs = async (req, res, next) => {
 };
 
 export const createJob = async (req, res, next) => {
-  const { company, position } = req.body;
   const job = await Job.create(req.body);
   res
     .status(StatusCodes.CREATED)
@@ -25,20 +22,15 @@ export const getJob = async (req, res, next) => {
   const { id } = req.params;
   const job = await Job.findById(id);
 
-  if (!job) throw new NotFoundError(`No job found with this ID : ${id}`);
-
   res.json({ status: 'success', message: 'ok', data: job });
 };
 
-export const editJob = async (req, res, next) => {
+export const editJob = async (req, res, next ) => {
   const { id } = req.params;
-  const { position, company } = req.body;
 
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-
-  if (!updatedJob) throw new NotFoundError(`No job found with this ID : ${id}`);
 
   res
     .status(StatusCodes.OK)
