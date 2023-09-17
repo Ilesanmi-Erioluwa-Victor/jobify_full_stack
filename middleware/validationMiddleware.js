018c8445a2ac0fb62c3b7c5cccce7fa7290ba763
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 21:24:37 +0100

    working on req.body validation

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -2,18 +2,16 @@ import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/customError.js';
 
 const withValidationErrors = (validateValues) => {
-    
-}
-
-[body('name').notEmpty().withMessage('Name is required')],
-  (req, res, next) => {
+    return [validateValues, (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const errorMessages = errors.array().map((error) => error.msg);
-      return res.status(400).json({
-        errors: errorMessages,
-      });
+      throw new BadRequestError(errorMessages)
     }
 
     next();
-  },
+  }]
+}
+
+[body('name').notEmpty().withMessage('Name is required')],
+  
