328b84c127a40b4dcc65b85858a60b54956c43ac
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 15:35:58 +0100

    working on Routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 
-
 dotenv.config();
 
 import jobRoute from './routes/job.routes';
@@ -13,28 +12,11 @@ process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : null;
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world');
-});
-
 app.post('/', (req, res, next) => {
   console.log(req);
 });
 
-// GET JOB
-app.get('/api/v1/jobs/:id');
-
-// DELETE JOB
-app.delete('/api/v1/jobs/:id');
-
-// EDIT JOB
-app.patch('/api/v1/jobs/:id');
-
-// CREATE JOB
-app.post('/api/v1/jobs');
-
-// ALL JOBS
-app.get('/api/v1/jobs');
+app.use('/api/v1/jobs', jobRoute);
 
 app.use('*', (req, res, next) => {
   res.status(404).json({
