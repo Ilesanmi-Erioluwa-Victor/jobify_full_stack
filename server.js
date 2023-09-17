b5acdbaf1615acb97e5f58d8982b7a08f55fff4b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 20:59:02 +0100

    working on req.body validation

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : null;
 
 app.use(express.json());
 
-app.post('/api/v1/test', [body('name')], (req, res, next) => {
+app.post('/api/v1/test', [body('name').notEmpty()], (req, res, next) => {
   const { name } = req.body;
 
   res.json({
