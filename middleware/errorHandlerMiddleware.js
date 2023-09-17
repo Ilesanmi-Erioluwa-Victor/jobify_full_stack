533a6be129348ebdb161d62b7d9658e93d11e05b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 18:00:54 +0100

    working on CustomError

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,9 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (error, req, res, next) => {
-    console.log(error);
-    const statusCode = 
-  res.status(500).json({
+  console.log(error);
+  const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+ 
+    res.status(500).json({
     message: 'something went wrong',
   });
   next();
