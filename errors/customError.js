7c87e8f114a986f70ad9d0d2d17251275bc1984b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-17 17:41:37 +0100

    working on CustomError

--- errors/customError.js
+++ errors/customError.js
@@ -1,7 +1,9 @@
-import { StatusCodes } from "http-status-codes";
+import { StatusCodes } from 'http-status-codes';
 
-export class NotFoundError extends Error{
-    constructor(message) {
-        super(message)
-    }
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = ' NotFoundError';
+    this.statusCodes = StatusCodes.NOT_FOUND;
+  }
 }
