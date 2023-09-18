4482c671a7242c7a4fbb9ae1898441d3acd11737
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 00:22:06 +0100

    workin on customSelect

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constant';
 import { customFetch } from 'utils/CustomFetch';
@@ -38,7 +38,12 @@ const AddJob = () => {
             labelText={'job location'}
             defaultValue={user.location}
           />
-         
+          <FormRowSelect
+            labelText={'job status'}
+            name={'jobStatus'}
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
           <button
             type='submit'
             className='btn btn-block form-btn'
