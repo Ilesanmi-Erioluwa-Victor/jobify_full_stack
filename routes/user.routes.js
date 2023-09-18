60b2c33cdaee4437000c305af7a5c6cd634a3962
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 02:00:00 +0100

    working on Validation of register user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { login, register } from '../controller/authController.js';
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
 
 const route = Router();
-route.post('/register', register);
+route.post('/register', validateRegisterInput, register);
 route.post('/login', login);
 export default route;
