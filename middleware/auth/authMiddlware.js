e0aeee6887ac3d38dbb42c81783141690a956dbf
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 10:54:41 +0100

    working on verifyingToken

--- middleware/auth/authMiddlware.js
+++ middleware/auth/authMiddlware.js
@@ -1,4 +1,5 @@
 import { UnauthenticatedError } from '../../errors/customError.js';
+import { verifyJWT } from '../../utils/Token.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
@@ -6,6 +7,8 @@ export const authenticateUser = async (req, res, next) => {
   if (!token) throw new UnauthenticatedError('authentication failed');
 
   try {
+      const user = verifyJWT(token);
+      console.log(user)
     next();
   } catch (error) {
     throw new UnauthenticatedError('authentication failed');
