068cda8d1d0b5977ae105efa9fc441ddc4043a30
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 01:45:14 +0100

    working on authController

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,5 +2,6 @@ import { Router } from 'express';
 import { login, register } from '../controller/authController.js';
 
 const route = Router();
+route.post('/register', register);
 
 export default route;
