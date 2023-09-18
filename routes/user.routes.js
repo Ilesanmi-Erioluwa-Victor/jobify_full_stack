71050b15e20ceb3e7a30968c5f857c8e7b2ada10
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 17:42:30 +0100

    working on authorizePermissions

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
+
 import {
   getCurrentUser,
   getApplication,
   updateUser,
 } from '../controller/userController.js';
+
 import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
+import { authorizePermissions } from '../middleware/auth/authMiddlware.js';
 
 const route = Router();
 route.get('/current-user', getCurrentUser);
-route.get('/admin/app-stats', getApplication);
+route.get('/admin/app-stats', authorizePermissions('admin'), getApplication);
 route.patch('/update-user', validateUpdateUserInput, updateUser);
 
 export default route;
