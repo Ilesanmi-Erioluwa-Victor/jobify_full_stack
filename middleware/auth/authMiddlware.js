6c30f928df57aed11b7d791791d3bafd8fcc55b1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 10:46:45 +0100

    working on auths

--- middleware/auth/authMiddlware.js
+++ middleware/auth/authMiddlware.js
@@ -3,6 +3,6 @@ import { UnauthenticatedError } from "../../errors/customError.js";
 export const authenticateUser = async (req, res, next) => {
     const { token } = req.cookies;
     
-    if(!token) throw new UnauthenticatedError
+    if(!token) throw new UnauthenticatedError("authentication failed")
   next();
 };
