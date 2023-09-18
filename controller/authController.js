31964f4ed434c84d54e7a3eab49b774a1ef39009
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 01:50:18 +0100

    working on authController

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,13 @@
-import { StatusCodes } from "http-status-codes";
-import userModel from "../models/userModel.js";
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/userModel.js';
 
 export const register = async (req, res, next) => {
-  res.send('register');
+  const user = await User.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    data: user,
+  });
 };
 
 export const login = async (req, res, next) => {
