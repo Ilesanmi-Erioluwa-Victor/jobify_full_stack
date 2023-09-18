ff50125039bff27addd90c7d8a3166f8250fe2c2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 02:10:15 +0100

    working on authController

--- controller/authController.js
+++ controller/authController.js
@@ -3,6 +3,7 @@ import User from '../models/userModel.js';
 
 export const register = async (req, res, next) => {
     const isFirstAccount = await User.countDocuments() === 0
+    req.body.role = isFirstAccount ? "admin" : "user"
   const user = await User.create(req.body);
 
   res.status(StatusCodes.CREATED).json({
