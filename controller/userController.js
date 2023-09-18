f44d309a5cfc243c6e7df6feb57bdce06a8a1657
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 17:29:44 +0100

    working on update-user

--- controller/userController.js
+++ controller/userController.js
@@ -19,6 +19,7 @@ export const getApplication = async (req, res, next) => {
 export const updateUser = async (req, res, next) => {
   const obj = { ...req.body };
   delete obj.password;
+  console.log(obj);
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({
     msg: 'update user',
