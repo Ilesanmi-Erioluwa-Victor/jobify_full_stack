925f3681179582c102d0bb042874c331db8ee2c5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 17:08:37 +0100

    working on getCurrentUser

--- controller/userController.js
+++ controller/userController.js
@@ -4,8 +4,9 @@ import Job from '../models/jobModel.js';
 
 export const getCurrentUser = async (req, res, next) => {
   const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
   res.status(StatusCodes.OK).json({
-    data: user,
+    data: userWithoutPassword,
   });
 };
 
