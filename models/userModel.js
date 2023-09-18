2f2ce69af40acd3895ba67736b9ea3edc0bf59d9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 01:32:40 +0100

    working on userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import { USER_ROLE } from '../utils/constant.js';
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    lastName: {
-        type: String,
-        default : "lastName"
-    },
-    location: {
-        type: String,
-        default : "my city"
-    },
-    role: {
-        type: String,
-        enum : 
-    }
-})
+  name: String,
+  email: String,
+  password: String,
+  lastName: {
+    type: String,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    default: 'my city',
+  },
+  role: {
+    type: String,
+    enum: Object.values(USER_ROLE),
+    default: USER_ROLE.USER,
+  },
+});
