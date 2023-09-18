14fa3cbda664677c41dc5653195e913fd4a52cc9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 16:39:19 +0100

    working on logout route

--- controller/userController.js
+++ controller/userController.js
@@ -2,3 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
 
+export const getCurrentUser = async (req, res, next) => {
+    res.status(StatusCodes.OK).json({
+        msg : "get Current user"
+    })
+}
