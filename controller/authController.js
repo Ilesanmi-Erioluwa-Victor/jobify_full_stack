a0b09cbd5753e0f69c0d881b925f6c3eb58f7c97
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 02:28:35 +0100

    working on hashing of password

--- controller/authController.js
+++ controller/authController.js
@@ -1,14 +1,14 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
-import bcrypt from 'bcryptjs';
+import { hashedPassword } from '../utils/Password.js';
 
 export const register = async (req, res, next) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
-  
-  
-    req.body.password = hashedPassword;
-    
+
+  const hashedPassword = await hashedPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({
     status: 'success',
