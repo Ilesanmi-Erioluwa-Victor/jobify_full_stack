31efa9c9d5cafd45e48a333bab2a9b43dbff818b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 16:48:43 +0100

    working on user aspect of the app

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,8 +6,8 @@ import {
 } from '../controller/userController.js';
 
 const route = Router();
-route.get('/current-user');
-route.get('/admin/app-stats');
-route.get('/update-user');
+route.get('/current-user', getCurrentUser);
+route.get('/admin/app-stats', getApplication);
+route.get('/update-user', updateUser);
 
 export default route;
