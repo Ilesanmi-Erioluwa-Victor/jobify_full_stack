0e0e787e8d70c2537060ca72632e0df5d3aec2d9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 17:00:21 +0100

    working on getCurrentUser

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,13 @@
-import { StatusCodes } from "http-status-codes";
-import User from "../models/userModel.js";
-import Job from "../models/jobModel.js";
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/userModel.js';
+import Job from '../models/jobModel.js';
 
 export const getCurrentUser = async (req, res, next) => {
-    res.status(StatusCodes.OK).json({
-        msg : "get Current user"
-    })
-}
+  const user = await User.findOne({ _id: req.user.userId });
+  res.status(StatusCodes.OK).json({
+    data: user,
+  });
+};
 
 export const getApplication = async (req, res, next) => {
   res.status(StatusCodes.OK).json({
