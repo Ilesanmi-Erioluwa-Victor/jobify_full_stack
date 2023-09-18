ce34ab9b9a35c5face6b98a4e5a79281911fc3ef
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-18 20:55:11 +0100

    working on customFetch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
 import App from './App.jsx';
 import './index.css';
+import { customFetch } from 'utils/CustomFetch.jsx';
 
-const data = await axios.get('/api/v1/test');
+const data = await customFetch.get('/api/v1/test');
 console.log(data);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
