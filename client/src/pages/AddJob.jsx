4d396b149509eb51858ccf401c65981fb8e38222
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 01:39:06 +0100

    working on create job

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -17,6 +17,7 @@ export const action = async ({ request }) => {
   try {
     await customFetch.post('/jobs', data);
     toast.success('Job added successfully');
+    return redirect('all-jobs');
   } catch (error) {
     toast.error(error?.response?.data?.msg);
     return error;
