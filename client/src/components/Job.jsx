4623a8d97d39478015868835570be1e4c6a84135
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 10:09:54 +0100

    working on edit job

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -45,7 +45,7 @@ const Job = ({
 
                   <div className={`status ${jobStatus}`}>{jobStatus}</div>
                   <footer className='actions'>
-                      <Link className='btn edit-btn'>Edit</Link>
+                      <Link className='btn edit-btn' to={`../edit-job${_id}`}>Edit</Link>
                       <Form>
                           <button type="submit" className='btn delete-btn'>
                               Delete
