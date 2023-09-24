b9054f34687dfb20cc210b2086ccff2978372114
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 02:21:22 +0100

    working on  Job

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,8 +1,10 @@
-import { Form, Link } from "react-router-dom";
-import day from "dayjs"
-import advancedFormat from "dayjs/plugin/advancedFormat"
-import JobInfo from "./JobInfo";
-import { FaLocationArrow, FaBriefcase, FaCalenderAlt } from "react-icons/fa";
+import { Form, Link } from 'react-router-dom';
+import day from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import JobInfo from './JobInfo';
+import { FaLocationArrow, FaBriefcase, FaCalenderAlt } from 'react-icons/fa';
+import Wrapper from 'assets/wrappers/Job';
+
 const Job = () => {
   return <h3>JOB</h3>;
 };
