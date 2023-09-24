0769940675fa6be4a341f2a24198f1fd7d796512
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 02:31:32 +0100

    working on  Job

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -2,12 +2,28 @@ import { Form, Link } from 'react-router-dom';
 import day from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import JobInfo from './JobInfo';
-import { FaLocationArrow, FaBriefcase, FaCalenderAlt } from 'react-icons/fa';
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
 import Wrapper from 'assets/wrappers/Job';
 day.extend(advancedFormat);
 
-const Job = () => {
-  return <h3>JOB</h3>;
+const Job = ({
+  _id,
+  company,
+  createdAt,
+  jobLocation,
+  jobStatus,
+  jobType,
+  postion,
+}) => {
+  const date = day(createdAt).format('MMM Do, YYYY');
+  return (
+    <Wrapper>
+      <header>
+              <div className='main-icon'>{company.charAt(0)}</div>
+              <div className="info"></div>
+      </header>
+    </Wrapper>
+  );
 };
 
 export default Job;
