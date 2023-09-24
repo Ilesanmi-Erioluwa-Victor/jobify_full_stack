222930104012f176cae6f1795443cb28e043bc9c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 02:06:53 +0100

    working on all Jobs

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,9 +1,18 @@
-import Job from "./Job";
-import Wrapper from "assets/wrappers/BigSidebar";
+import { useAllJobsContext } from 'pages/AllJobs';
+import Job from './Job';
+import Wrapper from 'assets/wrappers/JobsContainer';
 
 const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+  const jobResult = data;
+  if (!jobResult.length) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display</h2>
+      </Wrapper>
+    );
+  }
   return <h2>JobsContainer</h2>;
-}
+};
 
-export default JobsContainer
-
+export default JobsContainer;
