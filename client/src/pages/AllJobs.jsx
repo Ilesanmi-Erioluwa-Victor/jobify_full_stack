0c0829cf5bf929d30c12b9927109c925bd8cdac7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 01:46:05 +0100

    working on create job

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,5 +1,8 @@
 import { JobsContainer, SearchContainer } from "components";
+import { createContext, useContext } from "react";
+import { useLoaderData } from "react-router";
 import { toast } from "react-toastify";
+import { customFetch } from "utils/CustomFetch";
 
 const AllJobs = () => {
   return <div>AllJobs</div>;
