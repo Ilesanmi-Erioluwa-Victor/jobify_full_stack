f062f79426d605beb825fde57dd77a0e0e605c34
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 10:15:48 +0100

    working on edit job

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,11 +1,20 @@
-import Wrapper from "assets/wrappers/DashboardFormPage";
-import { FormRow, FormRowSelect } from "components";
-import { Form, useLoaderData, useNavigation, useParams } from "react-router-dom";
-red
+import Wrapper from 'assets/wrappers/DashboardFormPage';
+import { FormRow, FormRowSelect } from 'components';
+import {
+  Form,
+  redirect,
+  useLoaderData,
+  useNavigation,
+  useParams,
+} from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constant';
+import { toast } from 'react-toastify';
+import { customFetch } from 'utils/CustomFetch';
+
 
 const EditJob = () => {
-   const { id } = useParams();
+  const { id } = useParams();
   return <div>EditJob</div>;
-}
+};
 
-export default EditJob
+export default EditJob;
