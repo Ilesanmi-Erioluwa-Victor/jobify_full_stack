a9da5ce627273ff1280f3ae0f008ca1cf6e07d51
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-24 11:31:15 +0100

    working on edit job

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,6 +1,20 @@
+import Wrapper from 'assets/wrappers/StatsContainer';
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { customFetch } from 'utils/CustomFetch';
 
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats');
+
+    return response.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return redirect('/dashboard');
+  }
+};
 const Admin = () => {
-  return <div>AdminPage</div>;
-}
+  return <Wrapper>AdminPage</Wrapper>;
+};
 
-export default Admin
+export default Admin;
