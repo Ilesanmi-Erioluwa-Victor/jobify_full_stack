12390e13fe962c63a79e772a7351860cf2ca013b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-25 19:59:42 +0100

    working on multer storage

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,9 +1,11 @@
-import multer from "multer"
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/uploads")
-    }
-    
-    
-})
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads');
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, 'public/uploads');
+  },
+});
