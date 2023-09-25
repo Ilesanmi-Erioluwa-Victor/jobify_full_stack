3869cf0c1f306fe08fc7c1d701210099c1edd172
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-25 19:52:10 +0100

    modifying user Schema

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,7 +19,7 @@ export const action = async ({ request }) => {
     await customFetch.patch('/users/update-user', formData);
     toast.success('Profile updated successfully');
   } catch (error) {
-    
+    toast.error(error?.response?.data?.msg);
   }
 
   return null;
