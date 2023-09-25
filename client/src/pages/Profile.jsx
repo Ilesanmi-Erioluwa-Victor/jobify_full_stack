b5227399ec7b07a2ea5ef24901939bbfe726e0bb
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-25 19:26:05 +0100

    working on Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,10 @@
+import Wrapper from "assets/wrappers/DashboardFormPage";
+import { FormRow } from "components";
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import { toast } from "react-toastify";
+import { customFetch } from "utils/CustomFetch";
+
+
 const Profile = () => {
   return <div>Profile</div>;
 }
