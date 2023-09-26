87136e7274254fd60a972001e7d871fb4597eacd
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-26 20:20:11 +0100

    woeking on testUser

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -10,14 +10,18 @@ import {
   validateJobInput,
   validateIdParam,
 } from '../middleware/validationMiddleware.js';
+import { checkForTestUser } from '../middleware/auth/authMiddlware.js';
 
 const route = Router();
-route.route('/').get(getAllJobs).post(validateJobInput, createJob);
+route
+  .route('/')
+  .get(getAllJobs)
+  .post(checkForTestUser, validateJobInput, createJob);
 
 route
   .route('/:id')
   .get(validateIdParam, getJob)
-  .patch(validateJobInput, validateIdParam, editJob)
-  .delete(validateIdParam, deleteJob);
+  .patch(checkForTestUser, validateJobInput, validateIdParam, editJob)
+  .delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default route;
