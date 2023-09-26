32b8437c775a12d67536dfc84bae2d9a559ac06c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-26 21:36:05 +0100

    working on populate.json

--- populate.js
+++ populate.js
@@ -4,5 +4,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import Job from "./models/jobModel"
-import User from "./models/userModel"
+import Job from './models/jobModel';
+import User from './models/userModel';
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  const user = await User.findOne({ email: 'test@test.com' });
+  const jsonJob = JSON.parse(
+    await readFile(new URL('./utils/MOCK_DATA.json', import.meta.url))
+  );
+  const jobs = jsonJob.map((job) => {
+    return { ...job, createdBy: user_id };
+  });
+} catch (error) {}
