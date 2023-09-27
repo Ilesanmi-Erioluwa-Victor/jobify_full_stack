e37b24d97f8465c532eb20f154fea3904906f7fd
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 20:39:39 +0100

    working on Search container

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,11 +1,11 @@
-import Wrapper from "assets/wrappers/DashboardFormPage";
-import { FormRow, FormRowSelect, SubmitBtn } from "../components"
+import Wrapper from 'assets/wrappers/DashboardFormPage';
+import { FormRow, FormRowSelect, SubmitBtn } from '../components';
 import { Form, useSubmit, Link } from 'react-router-dom';
-
+import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from '../../../utils/constant';
+import { useAllJobsContext } from 'pages/AllJobs';
 
 const SearchContainer = () => {
   return <h2>SearchContainer</h2>;
-}
+};
 
-export default SearchContainer
-
+export default SearchContainer;
