b0984e13d7ed20621c4c0c991f9fcc921f32e0b5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 16:44:30 +0100

    working on StatsContainer

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,15 +1,19 @@
 import {
   ResponsiveContainer,
   BarChart,
-  Area,
+  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
 } from 'recharts';
 
-const BarChat = () => {
-  return <div>BarChat</div>;
+const BarChat = ({data}) => {
+  return <ResponsiveContainer width="100%" height={300}>
+    <BarChart data={data} margin={{top: 50}}>
+      
+ </BarChart>
+  </ResponsiveContainer>;
 };
 
 export default BarChat;
