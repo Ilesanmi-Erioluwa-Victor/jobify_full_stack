73e4752f65893de8ffd93cfabfe8656722068a0d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 16:45:47 +0100

    working on StatsContainer

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -8,12 +8,20 @@ import {
   Tooltip,
 } from 'recharts';
 
-const BarChat = ({data}) => {
-  return <ResponsiveContainer width="100%" height={300}>
-    <BarChart data={data} margin={{top: 50}}>
-      
- </BarChart>
-  </ResponsiveContainer>;
+const BarChat = ({ data }) => {
+  return (
+    <ResponsiveContainer
+      width='100%'
+      height={300}
+    >
+      <BarChart
+        data={data}
+        margin={{ top: 50 }}
+      >
+        <CartesianGrid strokeDasharray={'3 3'} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default BarChat;
