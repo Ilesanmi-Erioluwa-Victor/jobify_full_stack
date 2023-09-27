2f83edf744c7abafd5deb2f2348231d14648b1c5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 15:08:24 +0100

    working on Chart at the frontend

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,5 +1,9 @@
+import { ChartsContainer, StatsContainer } from 'components';
+import { useLoaderData } from 'react-router-dom';
+import { customFetch } from 'utils/CustomFetch';
+
 const Stats = () => {
   return <div>Stats</div>;
-}
+};
 
-export default Stats
+export default Stats;
