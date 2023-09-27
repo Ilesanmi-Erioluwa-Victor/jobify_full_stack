1925ff52bd8d73dc83330c3d343766cfbbd17865
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 18:25:05 +0100

    working on queryObject

--- controller/jobController.js
+++ controller/jobController.js
@@ -39,10 +39,12 @@ export const getAllJobs = async (req, res, next) => {
 
   const sortKey = sortOption[sort] || sortOption.newest;
 
-const page = Number(req.query.page) || 1
-
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
   const totalJobs = await Job.countDocuments(queryObject);
+  const skip = (page - 1) * limit;
 
+  
   const jobs = await Job.find(queryObject).sort(sortKey).skip(1).limit(2);
 
   res.status(StatusCodes.OK).json({
