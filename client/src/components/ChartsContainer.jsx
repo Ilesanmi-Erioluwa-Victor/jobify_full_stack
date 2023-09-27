be44d36359ed44f150d9e5ecf7f1792c130809d9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 15:52:05 +0100

    working on Charts Container

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -9,10 +9,19 @@ import Wrapper from 'assets/wrappers/ChartsContainer';
 const ChartsContainer = ({ data }) => {
   const [barChart, setBarChart] = useState(true);
 
-    return <Wrapper>
-        <h4>Monthly Applications</h4>
-        
-  </Wrapper>;
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button
+        type='button'
+        onClick={setBarChart(!barChart)}
+      >
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+          </button>
+          
+          {barChart ? <BarChat data={data}/>}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
