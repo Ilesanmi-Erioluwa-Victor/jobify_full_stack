b25cfc581bcf57db4ea6e51755e5c93a8c5c958c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 22:41:19 +0100

    working on PageBtnContainer

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,4 +1,7 @@
+import Wrapper from 'assets/wrappers/PageBtnContainer';
+import { useAllJobsContext } from 'pages/AllJobs';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
 
 const PageBtnContainer = () => {
   return <h1>PageBtnContainer</h1>;
