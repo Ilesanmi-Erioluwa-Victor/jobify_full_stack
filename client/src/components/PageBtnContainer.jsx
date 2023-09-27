45b7b7df4c973915343e2242b42084937ba12e7d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-27 22:44:50 +0100

    working on PageBtnContainer

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -7,6 +7,8 @@ const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
+
+  console.log(numOfPages, currentPage);
   return <h1>PageBtnContainer</h1>;
 };
 
